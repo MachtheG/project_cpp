6ab2ed807807336ddf431f44b3a612a7e1b5db87
machtheg <thayu.ndegwa@strathmore.edu>
2024-06-17 12:58:33 +0300

    In this commit:
    1. Activity class is defined
    2. variable definitions: 1. name 2. max_capacity 3. vector<Student> students
    3. Another constructor similar to the one in student class. Initializing the object activity, n , and cap.
    
    4. First function, addStudent.
    5. Second function for getting the remaining capacity.

--- main.cpp
+++ main.cpp
@@ -27,10 +27,27 @@ public:
         : firstname(fn), surname(sn), gender(g), age(a), group(grp) {}
 };
 
-//
+//Activity Class
+class Activity{
+public:
+    string name;
+    int max_capacity;
+    vector<Student> students;
 
-int main() {
+    Activity(string n, int cap) : name(n), max_capacity(cap) {}
+
+    bool addStudent(const Student& student){
+        if (students.size() < max_capacity){
+            students.push_back(student);
+            return true;
+        }
+        return false;
+    }
+
+    int getRemainingCapacity() {
+        return max_capacity - students.size();
+    }
 
 
-    return 0;
-}
+};
+
