84a8593fcf2a7feb7a3508078007b8cee746a629
machtheg <thayu.ndegwa@strathmore.edu>
2024-06-17 12:30:37 +0300

    Student class implementation.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
 using namespace std;
+
+
+//Classes Implementation
+// 1. Student Class -> To store details and selected activities.
+
+class Student {
+
+
+
+};
+
+
+
+
 
 int main() {
     cout << "Student Module: \t" << endl;
+
     return 0;
 }
